d48da06e006eca79b82017bc73b46e25e9a3b576
Liam Rivers <lrivers42@tntech.edu>
2025-03-06 12:51:18 -0600

    Created app.js
    Moved all jquery code from index.html to app.js to better work and focus on js in a separate file.

---
+++ app.js
@@ -0,0 +1,94 @@
+
+
+document.querySelector("#btnLogin").addEventListener("click",(e) => {
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    let strUsername = document.querySelector("#txtUsername").value
+    const strPassword = $('#txtPassword').val()
+    strUsername = strUsername.toLowerCase()
+    let blnError = false
+    let strMessage = ''
+
+    if(strPassword.length < 1){
+        blnError = true
+        strMessage += '<p class="mb-0 mt-0">Password Cannot Be Blank</p>'
+    }
+
+    if(!regEmail.test(strUsername)){
+        blnError = true
+        strMessage += '<p  class="mb-0 mt-0">Username must be an email address</p>'
+    }
+
+    if(blnError == true){
+        Swal.fire({
+            title: "Oh no, you have an error!",
+            html: strMessage,
+            icon: "error"
+        });
+    }
+    
+    if(blnError == false){
+        Swal.fire({
+            title: "Success!",
+            text: "success!",
+            icon: "success"
+        });
+    }
+
+})
+
+document.querySelector("#btnSubmit").addEventListener("click",(e) => {
+
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    let strUsername = document.querySelector("#txtUsername").value
+    const strPassword = $('#txtPassword').val()
+    let strFirst = document.querySelector("#txtFirstName").value
+    let strLast = document.querySelector("#txtLastName").value
+
+    strUsername = strUsername.toLowerCase()
+
+    let blnError = false
+    let strMessage = ''
+
+    if(strPassword.length < 1){
+        blnError = true
+    }
+
+    if(!regEmail.test(strUsername)){
+        blnError = true
+    }
+
+    if(strLast.length < 1){
+        blnError = true
+    }
+
+
+    if(blnError){
+
+        strMessage += "Please fill out all required parts of the form!"
+
+        Swal.fire({
+            title: "Whoops!",
+            html: strMessage,
+            icon: "error"
+        });
+    }
+
+    if(!blnError){
+        Swal.fire({
+            title: "Success!",
+            text: "success!",
+            icon: "success"
+        });
+    }
+
+})
+
+$('#btnSwapToRegister').on('click',function(){
+    $('#frmLogin').slideUp('fast')
+    $('#frmRegister').slideDown('slow')
+})
+
+$('#btnSwapToLogin').on('click',function(){
+    $('#frmRegister').slideUp('fast')
+    $('#frmLogin').slideDown('slow')
+})
